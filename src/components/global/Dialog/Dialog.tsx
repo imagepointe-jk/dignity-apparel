60d67dc4e6737694452a3365031e8a2144af2fb2
imagepointe-jk <148394472+imagepointe-jk@users.noreply.github.com>
2024-11-13 10:55:58 -0600

    Make dialog close when url changes

--- src/components/global/Dialog/Dialog.tsx
+++ src/components/global/Dialog/Dialog.tsx
@@ -1,8 +1,9 @@
 //Adapted from Coding In Public
 //https://github.com/coding-in-public/react-dialog-walkthrough/blob/main/src/components/Dialog.tsx
-import { forwardRef } from "react";
+import { forwardRef, useEffect } from "react";
 import styles from "@/styles/global/Dialog.module.css";
 import { XMark } from "@/components/icons/XMark";
+import { usePathname } from "next/navigation";
 
 type Props = {
   className?: string;
@@ -12,6 +13,15 @@ type Props = {
 
 const Dialog = forwardRef<HTMLDialogElement, Props>(
   ({ children, className, toggleDialog }, ref) => {
+    const pathname = usePathname();
+
+    useEffect(() => {
+      //@ts-expect-error: "current" does not exist
+      if (ref?.current.hasAttribute("open")) {
+        toggleDialog();
+      }
+    }, [pathname]);
+
     return (
       <dialog
         ref={ref}
