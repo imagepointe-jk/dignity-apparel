f50f3b1d7bfa2cb2c628c938281904044cbd5e52
imagepointe-jk <148394472+imagepointe-jk@users.noreply.github.com>
2024-12-10 09:23:33 -0600

    Add specific clear filter buttons

--- src/components/ProductBrowse/Filters/Filters.tsx
+++ src/components/ProductBrowse/Filters/Filters.tsx
@@ -4,6 +4,8 @@ import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { Suspense } from "react";
 import { ExpandableDiv } from "../../global/ExpandableDiv/ExpandableDiv";
 import { FilterGroup } from "./FilterGroup";
+import { searchParamsArray } from "@/utility/url";
+import { XMark2 } from "@/components/icons/XMark2";
 
 export type FilterItemType = {
   id: number;
@@ -46,6 +48,27 @@ export function FiltersWrapped({ categories, attributes }: Props) {
       )
       .map((attr) => attributeToFilterGroup(attr))
   );
+  const searchParamsArr = searchParamsArray(searchParams.toString());
+
+  //for each of the search params (some of which have multiple values), cross-reference with the filterGroups to build "clear button" data
+  const clearFilterButtons = searchParamsArr
+    .map((param) => {
+      const nameToMatch = param.key === "feature" ? "features" : param.key;
+      const matchingFilterGroup = filterGroups.find(
+        (group) => group.name === nameToMatch
+      );
+      return param.value.map((value) => {
+        const matchingSubItem = matchingFilterGroup?.subItems.find(
+          (sub) => sub.slug === value
+        );
+        return {
+          pairToClear: { key: param.key, value },
+          label: matchingSubItem?.name || "UNKNOWN",
+          id: `${matchingFilterGroup?.name}-${matchingSubItem?.id}`,
+        };
+      });
+    })
+    .flat();
 
   function attributeToFilterGroup(attribute: Attribute): FilterGroupType {
     return {
@@ -63,12 +86,38 @@ export function FiltersWrapped({ categories, attributes }: Props) {
     router.push(pathname);
   }
 
+  function clearParam(pair: { key: string; value: string }) {
+    const rawPairs = Array.from(new URLSearchParams(searchParams).entries());
+    const withoutPair = rawPairs.filter(
+      (raw) => !(raw[0] === pair.key && raw[1] === pair.value)
+    );
+    const newParams = new URLSearchParams();
+    for (const filteredPair of withoutPair) {
+      newParams.append(filteredPair[0], filteredPair[1]);
+    }
+    router.push(`${pathname}?${newParams}`);
+  }
+
   return (
     <div className={styles["main"]}>
       {searchParams.size > 0 && (
-        <button onClick={clearFilters} className={styles["clear-filters"]}>
-          Clear All Filters
-        </button>
+        <>
+          <button onClick={clearFilters} className={styles["clear-filters"]}>
+            Clear All Filters
+          </button>
+          <div className={styles["clear-filter-buttons-container"]}>
+            {clearFilterButtons.map((item) => (
+              <button
+                key={item.id}
+                className={styles["clear-filter-button"]}
+                onClick={() => clearParam(item.pairToClear)}
+              >
+                <XMark2 size={10} />
+                {item.label}
+              </button>
+            ))}
+          </div>
+        </>
       )}
       {filterGroups.map((group) => (
         <ExpandableDiv
--- src/utility/url.ts
+++ src/utility/url.ts
@@ -4,3 +4,20 @@ import { Product } from "@/types/schema/woocommerce";
 export function productUrl(product: Product, variationId?: number) {
   return `${env.NEXT_PUBLIC_BASE_URL}/products/${product.slug}${variationId !== undefined ? `?variationId=${variationId}` : ""}`;
 }
+
+export function searchParamsArray(
+  searchStr: string
+): { key: string; value: string[] }[] {
+  const arr: { key: string; value: string[] }[] = [];
+  const rawPairs = Array.from(new URLSearchParams(searchStr).entries());
+
+  for (const pair of rawPairs) {
+    const key = `${pair[0]}`;
+    const value = `${pair[1]}`;
+    const match = arr.find((item) => item.key === `${pair[0]}`);
+    if (match) match.value.push(`${pair[1]}`);
+    else arr.push({ key, value: [value] });
+  }
+
+  return arr;
+}
