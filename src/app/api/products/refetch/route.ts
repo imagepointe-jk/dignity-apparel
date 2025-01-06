34ccc6379d3e852910ee3ba7691fefc8e9d36087
imagepointe-jk <148394472+imagepointe-jk@users.noreply.github.com>
2025-01-06 09:02:45 -0600

    Test WC webhook

--- src/app/api/products/refetch/route.ts
+++ src/app/api/products/refetch/route.ts
@@ -1,7 +1,17 @@
 import { easyCorsInit } from "@/constants";
+import { NextRequest } from "next/server";
 
-export async function POST() {
+export async function POST(request: NextRequest) {
+  const webhookResource = request.headers.get("x-wc-webhook-source");
+  const webhookEvent = request.headers.get("x-wc-webhook-event");
+
+  if (
+    webhookResource === "product" &&
+    ["created", "updated", "deleted"].includes(`${webhookEvent}`)
+  ) {
+    console.log(`${webhookResource} event "${webhookEvent}" from WC`);
+  }
+
   //Always send a 200 response back to WooCommerce; the webhook seems to break otherwise
-  console.log("hello webhook");
   return Response.json({}, easyCorsInit);
 }
