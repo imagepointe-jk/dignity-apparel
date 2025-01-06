25496bd63bcda3fe5f514ff2a4414e04c391033e
imagepointe-jk <148394472+imagepointe-jk@users.noreply.github.com>
2025-01-06 08:56:20 -0600

    Remove unnecessary webhook route

--- src/app/api/products/refetch/webhook/route.ts
+++
@@ -1,7 +0,0 @@
-import { easyCorsInit } from "@/constants";
-
-export async function POST() {
-  //Always send a 200 response back to WooCommerce; the webhook seems to break otherwise
-  console.log("hello webhook");
-  return Response.json({}, easyCorsInit);
-}
