69e4b06963740e170437dc6d2b216aac654f671b
imagepointe-jk <148394472+imagepointe-jk@users.noreply.github.com>
2025-01-28 12:18:56 -0600

    Allow empty menu order field

--- src/types/validation/woocommerce/woocommerce.ts
+++ src/types/validation/woocommerce/woocommerce.ts
@@ -18,7 +18,7 @@ function pullProductData(productJson: any) {
     sku: productJson.sku,
     slug: productJson.slug,
     link: productJson.link,
-    menuOrder: productJson.menuOrder,
+    menuOrder: productJson.menuOrder || 0,
     imageUrl: productJson.image?.sourceUrl || "",
     descriptionSanitized: sanitizeHtml(productJson.description || ""),
     shortDescriptionSanitized: sanitizeHtml(productJson.shortDescription || ""),
